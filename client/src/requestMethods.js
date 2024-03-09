c2e9493c18ce722d65c467172a52cc0eb27a6adb
Tanmay Pandey <99367483+tanmayiitr17@users.noreply.github.com>
2024-03-09 22:17:20 +0530

    Update requestMethods.js

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://ecommerce-mern-gamma.vercel.app/";
 
 const TOKEN = localStorage.getItem("user-token");
 console.log(TOKEN)
