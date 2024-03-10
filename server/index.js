4fa2960033262184f5920a0ac693e91c9124749b
Tanmay Pandey <99367483+tanmayiitr17@users.noreply.github.com>
2024-03-10 05:54:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ mongoose
 app.use(cors(
   {
 <<<<<<< HEAD
-    origin: ["http://localhost:5173/"],
+    origin: ["http://localhost:5173/","https://ecommerce-mern-admin.vercel.app/","https://ecommerce-mern-client-17.vercel.app/"],
     methods: ["GET", "PUT", "POST", "DELETE"],
     credentials: true,
 =======
