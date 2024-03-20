25e823ad40fed1b97b7fa4d889c8216267861aef
Tanmay Pandey <99367483+tanmayiitr17@users.noreply.github.com>
2024-03-20 13:57:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,28 +30,28 @@ mongoose
 //   ],
 //   methods: ["GET", "PUT", "POST", "DELETE"]
 // }));
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5175",
-  "https://ecommerce-mern-admin.vercel.app",
-  "https://ecommerce-mern-client-17.vercel.app"
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:5175",
+//   "https://ecommerce-mern-admin.vercel.app",
+//   "https://ecommerce-mern-client-17.vercel.app"
+// ];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ["GET", "PUT", "POST", "DELETE"]
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ["GET", "PUT", "POST", "DELETE"]
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-// Handle preflight requests explicitly
-app.options('*', cors(corsOptions));
+// // Handle preflight requests explicitly
+// app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
