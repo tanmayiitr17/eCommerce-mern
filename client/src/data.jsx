d6d734d65126577b86f4737bd1712108bff858c7
Tanmay Pandey <tanmayiitroorkee17@gmail.com>
2024-06-01 01:34:26 +0530

    ui updated

--- client/src/data.jsx
+++ client/src/data.jsx
@@ -1,22 +1,22 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://i.ibb.co/cXFnLLV/3.png",
+    img: "https://cdn.create.vista.com/downloads/316a73d0-6135-49fa-9a30-e9658463b105_1024.jpeg",
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
   },
   {
     id: 2,
-    img: "https://i.ibb.co/DG69bQ4/2.png",
+    img: "https://cdn.create.vista.com/downloads/b6213872-1bc8-42f8-b6e5-752daf857948_1024.jpeg",
     title: "AUTUMN COLLECTION",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://i.ibb.co/cXFnLLV/3.png",
-    title: "LOUNGEWEAR LOVE",
+    img: "https://m.media-amazon.com/images/I/71YCZ6AXo1L._AC_UY1100_.jpg",
+    title: "KIDS LOVE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fbf0f4",
   },
@@ -25,18 +25,18 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "https://st.depositphotos.com/1007995/1274/i/450/depositphotos_12746726-stock-photo-fashion-man-wearing-sunglasses-thinking.jpg", 
+    img: "https://st.depositphotos.com/1007995/1274/i/450/depositphotos_12746726-stock-photo-fashion-man-wearing-sunglasses-thinking.jpg",
     title: "MEN STYLE!",
     cat: "MEN-STYLE",
   },
   {
-    id: 2, 
+    id: 2,
     img: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/21334304/2023/10/4/10d96212-4d28-447f-8ab7-cd15b91e503f1696406650695-anayna-Red-Floral-Ethnic-A-Line-Cotton-Midi-Ethnic-Dress-764-6.jpg",
     title: "WOMEN TREND!",
     cat: "WOMEN-TREND",
   },
   {
-    id: 3, 
+    id: 3,
     img: "https://img.freepik.com/free-photo/full-shot-kids-posing-together_23-2149853383.jpg",
     title: "KIDS FASHION!",
     cat: "KIDS-FASHION",
